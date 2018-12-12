43e7b8e9ca414103da75b1d55b1bc6e7138071b4
amit96321 <kumaramit96321@gmail.com>
2018-12-12 19:55:18 +0530

    Create bfs.cpp

---
+++ bfs.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void addEdge(vector<int> g[],int s,int e){
+    g[s].push_back(e);
+    //g[e].push_back(s); if graph is undirected
+}
+
+void bfsUtil(vector<int> g[],int s,int visited[]){
+    visited[s]=1;
+    vector<int> v;
+    v.push_back(s);
+    while(v.size()!=0){
+        cout<<v[0]<<" ";
+        for(int i=0;i<g[v[0]].size();i++){
+            if(visited[g[v[0]][i]]==0){
+                v.push_back(g[v[0]][i]);
+                visited[g[v[0]][i]]=1;
+            }
+        }
+        v.erase(v.begin());
+    }
+}
+
+void bfs(vector<int> g[],int size){
+    int visited[size]={0};
+    for(int i=2;i<size;i++)
+        if(visited[i]==0)
+            bfsUtil(g,2,visited);
+    return;
+}
+
+int main(){
+    vector<int> g[4];
+    addEdge(g,0, 1); 
+    addEdge(g,0, 2); 
+    addEdge(g,1, 2); 
+    addEdge(g,2, 0); 
+    addEdge(g,2, 3); 
+    addEdge(g,3, 3); 
+    bfs(g,4);
+}
